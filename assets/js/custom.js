27705528346fce61402be9b0a47f10ac72bbb25d
Pedro Janeiro <pjaneiro@whitesmith.co>
2019-10-01 09:55:07 +0100

    Remove script tag from js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,14 @@
+var classname = document.getElementsByClassName("link-landing");
 
-<script>
-    var classname = document.getElementsByClassName("link-landing");
+var linkHover = function() {
+    var attribute = 'm-theme--c'+this.getAttribute("id");
+    document.getElementById("hero-line").className = attribute;
+};
+var linkPlain = function() {
+    document.getElementById("hero-line").className = '';
+};
 
-    var linkHover = function() {
-        var attribute = 'm-theme--c'+this.getAttribute("id");
-        document.getElementById("hero-line").className = attribute;
-    };
-    var linkPlain = function() {
-        document.getElementById("hero-line").className = '';
-    };
-
-    for (var i = 0; i < classname.length; i++) {
-        classname[i].addEventListener('mouseover', linkHover, false);
-        classname[i].addEventListener('mouseout', linkPlain, false);
-    }
-
-</script>
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('mouseover', linkHover, false);
+    classname[i].addEventListener('mouseout', linkPlain, false);
+}
