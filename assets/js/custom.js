1371c6e4d8159f2edcac9109c76fcec815d6b0cd
Beatriz Beato <beatrizbeatosilva@gmail.com>
2019-10-01 08:22:57 +0100

    apply color to logo and lines on landing page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,18 @@
-// Custom scripts
+
+<script>
+    var classname = document.getElementsByClassName("link-landing");
+
+    var linkHover = function() {
+        var attribute = 'm-theme--c'+this.getAttribute("id");
+        document.getElementById("hero-line").className = attribute;
+    };
+    var linkPlain = function() {
+        document.getElementById("hero-line").className = '';
+    };
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('mouseover', linkHover, false);
+        classname[i].addEventListener('mouseout', linkPlain, false);
+    }
+
+</script>
